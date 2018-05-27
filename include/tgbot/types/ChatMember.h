1deb50e9db6e1b668af8c993b449d59f001c8977
JellyBrick <shlee1503@naver.com>
2018-05-27 16:35:52 +0900

    Bot API 3.1 update

--- include/tgbot/types/ChatMember.h
+++ include/tgbot/types/ChatMember.h
@@ -1,5 +1,6 @@
 //
 // Created by Konstantin Kukin on 26/12/16.
+// Edit by JellyBrick on 27/05/18.
 //
 
 #ifndef TGBOT_CHATMEMBER_H
@@ -26,9 +27,79 @@ public:
 	User::Ptr user;
 
 	/**
-	 * The member's status in the chat. Can be ìcreatorî, ìadministratorî, ìmemberî, ìleftî or ìkickedî
+	 * The member's status in the chat. Can be ‚Äúcreator‚Äù, ‚Äúadministrator‚Äù, ‚Äúmember‚Äù, ‚Äúrestricted‚Äù, ‚Äúleft‚Äù or ‚Äúkicked‚Äù
 	 */
 	std::string status;
+
+	/**
+	 * Optional. Restricted and kicked only. Date when restrictions will be lifted for this user, unix time.
+	 */
+	uint64_t untilDate;
+
+	/**
+	 * Optional. Administrators only. True, if the bot is allowed to edit administrator privileges of that user.
+	 */
+	bool canBeEdited = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can change the chat title, photo and other settings.
+	 */
+	bool canChangeInfo = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can post in the channel, channels only.
+	 */
+	bool canPostMessages = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can edit messages of other users and can pin messages, channels only.
+	 */
+	bool canEditMessages = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can delete messages of other users.
+	 */
+	bool canDeleteMessages = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can invite new users to the chat.
+	 */
+	bool canInviteUsers = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can restrict, ban or unban chat members.
+	 */
+	bool canRestrictMembers = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can pin messages, supergroups only.
+	 */
+	bool canPinMessages = false;
+
+	/**
+	 * Optional. Administrators only. True, if the administrator can add new administrators with a subset of his own privileges or demote administrators that he has promoted, directly or indirectly (promoted by administrators that were appointed by the user).
+	 */
+	bool canPromoteMembers = false;
+
+	/**
+	 * Optional. Restricted only. True, if the user can send text messages, contacts, locations and venues.
+	 */
+	bool canSendMessages = false;
+
+	/**
+	 * Optional. Restricted only. True, if the user can send audios, documents, photos, videos, video notes and voice notes, implies can_send_messages.
+	 */
+	bool canSendMediaMessages = false;
+
+	/**
+	 * Optional. Restricted only. True, if the user can send animations, games, stickers and use inline bots, implies can_send_media_messages.
+	 */
+	bool canSendOtherMessages = false;
+
+	/**
+	 * Optional. Restricted only. True, if user may add web page previews to his messages, implies can_send_media_messages.
+	 */
+	bool canAddWebPagePreviews = false;
 };
 }
 
