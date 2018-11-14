ba46cf2f3c9f253b84139f3b7229fe39fe5714ef
hannibal <770876997@qq.com>
2018-11-14 10:48:57 +0800

    add example

---
+++ samples/echobot-submodule/src/main.cpp
@@ -0,0 +1,46 @@
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
+
+#include <tgbot/tgbot.h>
+
+using namespace std;
+using namespace TgBot;
+
+int main() {
+    string token(getenv("TOKEN"));
+    printf("Token: %s\n", token.c_str());
+
+    Bot bot(token);
+    bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    bot.getEvents().onAnyMessage([&bot](Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+
+    signal(SIGINT, [](int s) {
+        printf("SIGINT got\n");
+        exit(0);
+    });
+
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        bot.getApi().deleteWebhook();
+
+        TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    } catch (exception& e) {
+        printf("error: %s\n", e.what());
+    }
+
+    return 0;
+}
