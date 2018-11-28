c6e0480998dd2759581f2ddf45c625d074d983eb
Artalus <artalus-mail@yandex.ru>
2018-11-28 22:33:31 +0300

    Move packager variables to build.py

--- build.py
+++ build.py
@@ -4,6 +4,14 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(
+        username="reo7sp",
+        login_username="artalus", # change to bintray account
+        upload="https://api.bintray.com/conan/artalus/conan-public", # change to bintray api
+        channel="ci",
+        stable_branch_pattern="release/*",
+        upload_only_when_stable=True,  # Will only upload when the branch matches "release/*"
+        docker_32_images=True
+    )
     builder.add_common_builds()
     builder.run()
