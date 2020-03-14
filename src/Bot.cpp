b780dfdf19b86b500295d43c19f8f8cf914b61b5
Egor Pugin <egor.pugin@gmail.com>
2020-03-14 18:09:00 +0300

    Init unique_ptr in C++11 style.

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -7,7 +7,9 @@ namespace TgBot {
 Bot::Bot(std::string token, const HttpClient& httpClient)
     : _token(std::move(token))
     , _api(_token, httpClient)
-    , _eventBroadcaster(std::make_unique<EventBroadcaster>())
+    // uncomment when c++14 is available
+    //, _eventBroadcaster(std::make_unique<EventBroadcaster>())
+    , _eventBroadcaster(new EventBroadcaster())
     , _eventHandler(getEvents()) {
 }
 
