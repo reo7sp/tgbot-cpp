c5cb76f8a482579555bdd48d65649bc6099d52e8
Egor Pugin <egor.pugin@gmail.com>
2020-03-15 14:44:08 +0300

    Remove workaround.

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -7,11 +7,7 @@ namespace TgBot {
 Bot::Bot(std::string token, const HttpClient& httpClient)
     : _token(std::move(token))
     , _api(_token, httpClient)
-#if __cplusplus > 201103L
     , _eventBroadcaster(std::make_unique<EventBroadcaster>())
-#else
-    , _eventBroadcaster(new EventBroadcaster())
-#endif
     , _eventHandler(getEvents()) {
 }
 
