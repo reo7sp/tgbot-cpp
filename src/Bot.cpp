a9806073ef7a6a14a9db4ab281c2910c7545229b
JellyBrick <shlee1503@naver.com>
2020-03-15 12:18:29 +0900

    std::make_unique support for version below C++14

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -2,14 +2,45 @@
 
 #include "tgbot/EventBroadcaster.h"
 
+#if __cplusplus < 201402L 
+namespace std {
+    template<class T> struct _Unique_if {
+        typedef unique_ptr<T> _Single_object;
+    };
+
+    template<class T> struct _Unique_if<T[]> {
+        typedef unique_ptr<T[]> _Unknown_bound;
+    };
+
+    template<class T, size_t N> struct _Unique_if<T[N]> {
+        typedef void _Known_bound;
+    };
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Single_object
+        make_unique(Args&&... args) {
+            return unique_ptr<T>(new T(std::forward<Args>(args)...));
+        }
+
+    template<class T>
+        typename _Unique_if<T>::_Unknown_bound
+        make_unique(size_t n) {
+            typedef typename remove_extent<T>::type U;
+            return unique_ptr<T>(new U[n]());
+        }
+
+    template<class T, class... Args>
+        typename _Unique_if<T>::_Known_bound
+        make_unique(Args&&...) = delete;
+}
+#endif
+
 namespace TgBot {
 
 Bot::Bot(std::string token, const HttpClient& httpClient)
     : _token(std::move(token))
     , _api(_token, httpClient)
-    // uncomment when c++14 is available
-    //, _eventBroadcaster(std::make_unique<EventBroadcaster>())
-    , _eventBroadcaster(new EventBroadcaster())
+    , _eventBroadcaster(std::make_unique<EventBroadcaster>())
     , _eventHandler(getEvents()) {
 }
 
