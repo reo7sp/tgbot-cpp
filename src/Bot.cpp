ff65d84e8d76d62e07984301d3d0b3ae548d5613
JellyBrick <shlee1503@naver.com>
2020-03-15 12:57:27 +0900

    Fix build error in versions below MSVC 12.0

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -2,36 +2,12 @@
 
 #include "tgbot/EventBroadcaster.h"
 
-#if __cplusplus < 201402L 
+#if __cplusplus == 201103L
 namespace std {
-    template<class T> struct _Unique_if {
-        typedef unique_ptr<T> _Single_object;
-    };
-
-    template<class T> struct _Unique_if<T[]> {
-        typedef unique_ptr<T[]> _Unknown_bound;
-    };
-
-    template<class T, size_t N> struct _Unique_if<T[N]> {
-        typedef void _Known_bound;
-    };
-
-    template<class T, class... Args>
-        typename _Unique_if<T>::_Single_object
-        make_unique(Args&&... args) {
-            return unique_ptr<T>(new T(std::forward<Args>(args)...));
-        }
-
-    template<class T>
-        typename _Unique_if<T>::_Unknown_bound
-        make_unique(size_t n) {
-            typedef typename remove_extent<T>::type U;
-            return unique_ptr<T>(new U[n]());
-        }
-
-    template<class T, class... Args>
-        typename _Unique_if<T>::_Known_bound
-        make_unique(Args&&...) = delete;
+    template<typename T>
+    inline std::unique_ptr<T> make_unique() {
+        return std::unique_ptr<T>(new T());
+    }
 }
 #endif
 
