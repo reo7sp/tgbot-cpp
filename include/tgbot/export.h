53e5e8cb0143736eb69820220a220a72cdd616de
Transporter <ogre.transporter@gmail.com>
2020-04-30 19:25:47 +0200

    Fix issure #138: DLL export fails

--- include/tgbot/export.h
+++ include/tgbot/export.h
@@ -2,7 +2,19 @@
 #define TGBOT_EXPORT_H
 
 #ifndef TGBOT_API
+#ifdef _MSC_VER
+#ifdef TGBOT_DLL
+#ifdef TgBot_EXPORTS
+#define TGBOT_API __declspec(dllexport)
+#else
+#define TGBOT_API __declspec(dllimport)
+#endif
+#else
 #define TGBOT_API
+#endif
+#else
+#define TGBOT_API
+#endif
 #endif
 
 #endif //TGBOT_EXPORT_H
